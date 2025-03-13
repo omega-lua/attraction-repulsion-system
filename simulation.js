478a092cb7b1dc1afad3bee0029e239c30b9cd0d
omega-lua <140018816+omega-lua@users.noreply.github.com>
2025-03-13 23:07:08 +0100

    init

---
+++ simulation.js
@@ -0,0 +1,97 @@
+
+// define variables for simulation
+let n = 5;
+let nSpecies = 2;
+let particle = [null, null, null, null, null] //[x, y, vx, vy, s(pecies)]
+let Particles = []
+
+// visual canvas settings
+let lastUpdate = 0;
+let updateInterval = 1000 / 1; // 30 updates per second
+let particleSize = 50
+
+//setting up canvas
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// initial function for creating particles in an array.
+function createParticles() {
+  for (let i = 0; i < n; i++) {
+    // Create a particle with random values
+    let particle = {
+      x: Math.random() * 100, // Random x position between 0 and 100
+      y: Math.random() * 100, // Random y position between 0 and 100
+      vx: Math.random() * 10 - 5, // Random velocity between -5 and 5
+      vy: Math.random() * 10 - 5, // Random velocity between -5 and 5
+      t: Math.random() // Not good code
+    };
+    
+    Particles.push(particle);
+  }
+
+  return Particles;
+}
+
+
+function updateSimulation() {
+  let distances = []
+  //calculate for every particle
+  for (let i = 0; i < n-1; i++) {
+
+    //calculate with every other particle
+    for (let j = i+1; j < n; j++) {
+      if (j == i) continue; //dont calculate with the particle itself
+      let dx = Particles[i].x - Particles[j].x;
+      let dy = Particles[i].y - Particles[j].y;
+      let d = Math.sqrt(dx**2 + dy**2);
+
+      distances.push(d)
+    }
+  }
+  console.log(distances);
+  return distances
+}
+
+// function for drawing a particle.
+function drawParticle(particle) {
+  ctx.beginPath();
+  ctx.arc(particle.x, particle.y, particleSize, 0, Math.PI * 2);
+  ctx.fillStyle = "white";
+  ctx.fill();
+}
+
+// upates visuals at every frame
+function updateCanvas() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height); //clear canvas
+
+  for (let i = 0; i < n; i++) { //draw particles
+    drawParticle(Particles[i])
+  }
+}
+
+function loopSimulation(timestamp) {
+    // updates simulation after a certain period of time.
+    if (timestamp - lastUpdate >= updateInterval) {
+      
+      // update simulation
+      updateSimulation()
+
+      // update canvas
+      updateCanvas()
+
+      lastUpdate = timestamp;
+    }
+  
+    // tell browser to call function at every frame
+    requestAnimationFrame(loopSimulation);
+}
+
+
+// run the program
+createParticles()
+loopSimulation()
+
+// log
+console.log(Particles);
