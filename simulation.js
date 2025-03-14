cd08cdb80255938e3b56bd922d2d3f72aa477f13
omega-lua <140018816+omega-lua@users.noreply.github.com>
2025-03-14 17:23:05 +0100

    Update simulation.js

--- simulation.js
+++ simulation.js
@@ -1,6 +1,5 @@
-
 // define variables for simulation
-let n = 5;
+let n = 3;
 let nSpecies = 2;
 let particle = [null, null, null, null, null] //[x, y, vx, vy, s(pecies)]
 let Particles = []
@@ -36,20 +35,34 @@ function createParticles() {
 
 
 function updateSimulation() {
+  
+  //calculate distances between each particle
   let distances = []
-  //calculate for every particle
+  let vectors = []
   for (let i = 0; i < n-1; i++) {
-
-    //calculate with every other particle
     for (let j = i+1; j < n; j++) {
-      if (j == i) continue; //dont calculate with the particle itself
+      // calculate distance
       let dx = Particles[i].x - Particles[j].x;
       let dy = Particles[i].y - Particles[j].y;
       let d = Math.sqrt(dx**2 + dy**2);
-
+      
+      //calculate force vector
+      let vector = [dx, dy]
+      let nVector = [(dx/d), (dy/d)]
+      
+      //calculate force
+      // ...
+      
       distances.push(d)
     }
   }
+  
+  //calculate force between each particle
+  function gravitationalForce(d) {
+  	let f = 1/d
+    return f
+  }
+  
   console.log(distances);
   return distances
 }
